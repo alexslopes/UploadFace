4f76f7b54b4d10a3e0f3ad7f4091dfb4fa2c80da
Alex <alexstlopes@gmail.com>
2019-03-09 22:24:32 -0300

    TReinamento e detecção simples funcionando

---
+++ app/src/main/java/com/example/uploadface/UploadFile.java
@@ -0,0 +1,91 @@
+package com.example.uploadface;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ContentBody;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class UploadFile extends AsyncTask<String, Void, String> {
+
+    public static final String TAG = "Photo";
+
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+
+    public AsyncResponse delegate = null;
+
+    public UploadFile(AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        String service = params[0];
+        File inFile = new File(params[1]);
+        String id = null;
+        FileInputStream fis = null;
+        String responseString = null;
+
+        if(params.length >= 3)
+            id = params[2];
+
+        try {
+            fis = new FileInputStream(inFile);
+            DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+
+            // server back-end URL
+            HttpPost httppost = new HttpPost(service);
+            MultipartEntity entity = new MultipartEntity();
+
+            // set the file input stream and file name as arguments
+            entity.addPart("file", new InputStreamBody(fis, inFile.getName()));
+            if(id != null)
+                entity.addPart("nome", new StringBody(id));
+            httppost.setEntity(entity);
+
+            // execute the request
+            HttpResponse response = httpclient.execute(httppost);
+
+            int statusCode = response.getStatusLine().getStatusCode();
+            HttpEntity responseEntity = response.getEntity();
+            responseString = EntityUtils.toString(responseEntity, "UTF-8");
+
+
+        } catch (ClientProtocolException e) {
+            System.err.println("Unable to make connection");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Unable to read file");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null) fis.close();
+            } catch (IOException e) {}
+        }
+
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+        delegate.processFinish(response);
+    }
+}
